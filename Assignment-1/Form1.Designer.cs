d1ed5e14aa335120a5f376262b5742cbf9e4025b
BibekPaudel43 <bibekpoudel43@gmail.com>
2018-03-15 15:48:00 +0545

    first commit.

---
+++ Assignment-1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Assignment_1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsStateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadStateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusBar = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 24);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(591, 410);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(619, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsImageToolStripMenuItem,
+            this.saveAsStateToolStripMenuItem,
+            this.loadStateToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveAsImageToolStripMenuItem
+            // 
+            this.saveAsImageToolStripMenuItem.Name = "saveAsImageToolStripMenuItem";
+            this.saveAsImageToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.saveAsImageToolStripMenuItem.Text = "Save as Image";
+            // 
+            // saveAsStateToolStripMenuItem
+            // 
+            this.saveAsStateToolStripMenuItem.Name = "saveAsStateToolStripMenuItem";
+            this.saveAsStateToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.saveAsStateToolStripMenuItem.Text = "Save as State";
+            // 
+            // loadStateToolStripMenuItem
+            // 
+            this.loadStateToolStripMenuItem.Name = "loadStateToolStripMenuItem";
+            this.loadStateToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.loadStateToolStripMenuItem.Text = "Load State";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 435);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(619, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(39, 17);
+            this.statusBar.Text = "Status";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 457);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsStateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadStateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusBar;
+    }
+}
+
---
+++ Assignment-1/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private  int MAX = 256;      // max iterations
+        private  double SX = -2.025; // start value real
+        private  double SY = -1.125; // start value imaginary
+        private  double EX = 0.6;    // end value real
+        private  double EY = 1.125;  // end value imaginary
+        private static int x1, y1, xs, ys, xe, ye;
+        private static double xstart, ystart, xende, yende, xzoom, yzoom;
+        private static bool action, rectangle, finished;
+        private static float xy;
+        private Image picture;
+        private Graphics g1;
+        private Cursor c1, c2;
+        private ToHSB HSBcol = new ToHSB();
+
+        //fires when form is loaded
+        private void Form_Load(object sender, EventArgs e)
+        {
+            init();
+            start();
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            graphics.DrawImage(picture, 0, 0);
+        }
+
+        private void initvalues() // reset start values
+        {
+            xstart = SX;
+            ystart = SY;
+            xende = EX;
+            yende = EY;
+            if ((float)((xende - xstart) / (yende - ystart)) != xy)
+                xstart = xende - (yende - ystart) * (double)xy;
+        }
+
+
+        // Load when the form loads
+        //all instances will be prepared
+        public void init()
+        {
+            finished = false;
+            c1 = Cursors.WaitCursor;
+            c2 = Cursors.Cross;
+            x1 = pictureBox.Size.Width;
+            y1 = pictureBox.Size.Height;
+            xy = (float)x1 / (float)y1;
+            picture = new Bitmap(x1,y1);
+            g1 = Graphics.FromImage(picture);
+            finished = true;
+        }
+
+        public void start()
+        {
+            action = false;
+            rectangle = false;
+            initvalues();
+            xzoom = (xende - xstart) / (double)x1;
+            yzoom = (yende - ystart) / (double)y1;
+            mandelbrot();
+        }
+
+        //for coloring the points 
+        private float pointcolour(double xwert, double ywert) // color value from 0.0 to 1.0 by iterations
+        {
+            double r = 0.0, i = 0.0, m = 0.0;
+            int j = 0;
+
+            while ((j < MAX) && (m < 4.0))
+            {
+                j++;
+                m = r * r - i * i;
+                i = 2.0 * r * i + ywert;
+                r = m + xwert;
+            }
+            return (float)j / (float)MAX;
+        }
+
+        
+        private void mandelbrot() // calculate all points
+        {
+            int x, y;
+            float h, b, alt = 0.0f;
+            Pen pn=null;
+            Color col;
+
+            action = false;
+            pictureBox.Cursor = c1;
+            statusBar.Text = ("Mandelbrot-Set will be produced - please wait...");
+            for (x = 0; x < x1; x += 2)
+                for (y = 0; y < y1; y++)
+                {
+                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y); // color value
+                    if (h != alt)
+                    {
+                        b = 1.0f - h * h; 
+                        //calling method of ToHSB class(passing value into)                   
+                        col = ToHSB.HSBtoRGB(h, 0.8f, b);
+                        pn = new Pen(col);
+                        //djm 
+                        alt = h;
+                    }
+                    g1.DrawLine(pn,x, y, x + 1, y);
+                }
+            statusBar.Text = ("Mandelbrot-Set ready - please select zoom area with pressed mouse.");
+            pictureBox.Cursor = c2;
+            action = true;
+        }
+
+        //fires when we press the mouse
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (action)
+            {
+                xstart = e.X;
+                ystart = e.Y;
+            }
+        }
+
+        //fires when we drag the mouse 
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (action)
+            {
+                xe = e.X;
+                ye = e.Y;
+
+                Graphics g = pictureBox.CreateGraphics();
+                update(g);
+            }
+        }
+
+        //fires when we release the pressed mouse 
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            int z, w;
+            if (action)
+            {
+                xe = e.X;
+                ye = e.Y;
+                if (xs > xe)
+                {
+                    z = xs;
+                    xs = xe;
+                    xe = z;
+                }
+                if (ys > ye)
+                {
+                    z = ys;
+                    ys = ye;
+                    ye = z;
+                }
+                w = (xe - xs);
+                z = (ye - ys);
+                if ((w < 2) && (z < 2)) initvalues();
+                else
+                {
+                    if (((float)w > (float)z * xy)) ye = (int)((float)ys + (float)w / xy);
+                    else xe = (int)((float)xs + (float)z * xy);
+                    xende = xstart + xzoom * (double)xe;
+                    yende = ystart + yzoom * (double)ye;
+                    xstart += xzoom * (double)xs;
+                    ystart += yzoom * (double)ys;
+                }
+                xzoom = (xende - xstart) / (double)x1;
+                yzoom = (yende - ystart) / (double)y1;
+                mandelbrot();
+                rectangle = false;
+                //repaint();
+            }
+        }
+
+        //
+        public void update(Graphics g)
+        {
+            Pen pen = new Pen(Color.White, 2);
+            g.DrawImage(picture, 0, 0);
+            if (rectangle)
+            {
+                
+                if (xs < xe)
+                {
+                    if (ys < ye) g.DrawRectangle(pen, xs, ys, (xe - xs), (ye - ys));
+                    else g.DrawRectangle(pen, xs, ye, (xe - xs), (ys - ye));
+                }
+                else
+                {
+                    if (ys < ye) g.DrawRectangle(pen, xe, ys, (xs - xe), (ye - ys));
+                    else g.DrawRectangle(pen, xe, ye, (xs - xe), (ys - ye));
+                }
+            }
+        }
+
+
+    }
+}
